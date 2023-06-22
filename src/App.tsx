82372d4f26b8efb9f1db5a70b0065f54a9f9168c
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-21 20:49:43 -0500

    Create a11y friendly form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mx-auto my-0 flex max-w-7xl justify-center p-8 text-black">
+      <div className="flex max-w-3xl basis-full flex-col gap-4 bg-slate-600 p-4">
+        <div className="flex">
+          <label htmlFor="ticket-numbers" className="sr-only">
+            Ticket Numbers
+          </label>
+          <input type="text" id="ticket-numbers" className="basis-full" />
+          <ul></ul>
+        </div>
+
+        <div className="flex gap-4">
+          <select name="type" id="type" className="basis-2/12">
+            <label htmlFor="type" className="sr-only" />
+            <option value="feat">feat</option>
+          </select>
+          <label htmlFor="scope" className="sr-only">
+            Scope
+          </label>
+          <input type="text" id="scope" className="basis-10/12" />
+        </div>
+
+        <div className="flex">
+          <label htmlFor="body" className="sr-only">
+            Body
+          </label>
+          <textarea
+            name="body"
+            id="body"
+            className="basis-full resize-none"
+          ></textarea>
+        </div>
+
+        <div className="flex">
+          <label htmlFor="footer" className="sr-only">
+            Footer
+          </label>
+          <textarea
+            name="footer"
+            id="footer"
+            className="basis-full resize-none"
+          ></textarea>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
