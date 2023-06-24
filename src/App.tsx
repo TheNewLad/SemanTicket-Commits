f6a534c2c54220874d02200a852d3047e590a5c1
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-24 09:12:58 -0500

    Import XCircleIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useTickets } from "./utils/hooks/useTickets.tsx";
 import { useForm } from "react-hook-form";
+import {
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
 
 function App() {
   const { register, resetField } = useForm();
@@ -34,20 +37,7 @@ function App() {
               <li key={id} className="flex gap-0.5 rounded-md bg-slate-400 p-1">
                 <span>{title}</span>
                 <button aria-label="Close" onClick={removeTicket(id)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
+                  <XCircleIcon className="h-6 w-6" />
                 </button>
               </li>
             ))}
