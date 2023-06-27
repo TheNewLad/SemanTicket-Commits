0e13d6eb45faa202e41dd755c405b8d0c686b145
Karim Dallas-Newell <22102599+TheNewLad@users.noreply.github.com>
2023-06-27 12:00:17 -0500

    refactor(commit form): improve ux (#7)
    * Move ticket input to match its usage
    
    * Show error when ticket number has more than 20 characters
    
    * Make method callable
    
    * Add plus button to add tickets
    
    * Clear tickets input error on change
    
    ---------
    
    Co-authored-by: devkdn <22102599+devkdn@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useTickets } from "./utils/hooks/useTickets.tsx";
 import { useForm } from "react-hook-form";
 import {
   ClipboardDocumentCheckIcon,
+  PlusIcon,
   XCircleIcon,
 } from "@heroicons/react/24/outline";
 import { ErrorMessage } from "./components/ErrorMessage.tsx";
@@ -12,18 +13,35 @@ function App() {
     register,
     resetField,
     watch,
-    formState: { isValid },
+    formState: { isValid, errors },
+    setError,
+    clearErrors,
+    trigger,
   } = useForm();
   const { tickets, addTickets, removeTicket } = useTickets();
 
-  // TODO: move to useTickets
+  const processTickets = (value: string) =>
+    addTickets(
+      value,
+      () => {
+        trigger("ticketNumberError").then(() => {
+          setError("ticketNumberError", {
+            type: "custom",
+          });
+        });
+      },
+      () => {
+        clearErrors("ticketNumberError");
+        resetField("ticket-numbers");
+      }
+    );
+
   const handleKeyUp = ({
     key,
     currentTarget: { value },
   }: React.KeyboardEvent<HTMLInputElement>) => {
     if (key === "Enter") {
-      addTickets(value);
-      resetField("ticket-numbers");
+      processTickets(value);
     }
   };
 
@@ -36,40 +54,13 @@ function App() {
   return (
     <div className="mx-auto my-0 flex max-w-7xl justify-center p-8 text-black">
       <div className="flex max-w-full basis-full flex-col gap-4 rounded-md border-2 border-gray-200 p-4 md:max-w-3xl">
-        <div className={`flex flex-col ${tickets.length ? "gap-4" : ""}`}>
-          <label htmlFor="ticket-numbers" className="sr-only">
-            Ticket Numbers
-          </label>
-          <input
-            {...register("ticket-numbers")}
-            type="text"
-            className="basis-full rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black"
-            placeholder="Ticket Number (optional): ABC-123, XYZ-789"
-            onKeyUp={(e) => handleKeyUp(e)}
-          />
-          <ul className="flex flex-wrap gap-1">
-            {tickets.map(({ id, title }) => (
-              <li key={id} className="flex gap-2">
-                <button
-                  aria-label="Close"
-                  onClick={removeTicket(id)}
-                  className="inline-flex gap-0.5 rounded-md border-2 border-blue-200 p-1 text-blue-200 transition hover:border-red-300 hover:text-red-300"
-                >
-                  <span>{title}</span>
-                  <XCircleIcon className="h-6 w-6" />
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-
         <div className="flex flex-col gap-4 md:flex-row">
           <label htmlFor="type" className="sr-only" />
           <select
             {...register("type", { required: "Type is required." })}
             name="type"
             id="type"
-            className="basis-2/12 rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black md:overflow-hidden"
+            className="basis-2/12 rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none md:overflow-hidden"
             defaultValue=""
           >
             <option value="" disabled>
@@ -101,7 +92,7 @@ function App() {
             {...register("scope")}
             type="text"
             id="scope"
-            className="basis-10/12 rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black"
+            className="basis-10/12 rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none"
             placeholder="Scope (optional): component name, file name, etc."
           />
         </div>
@@ -113,9 +104,56 @@ function App() {
           <input
             {...register("subject", { required: true })}
             type="text"
-            className="basis-full rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black"
+            className="basis-full rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none"
             placeholder="Subject: short description of the change"
           />
+        </div>
+
+        <div className={`flex flex-col ${tickets.length ? "gap-4" : ""}`}>
+          <label htmlFor="ticket-numbers" className="sr-only">
+            Ticket Numbers
+          </label>
+          <div className="relative flex">
+            <input
+              {...register("ticket-numbers")}
+              type="text"
+              className={`basis-full rounded-md border-2 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none ${
+                errors.ticketNumberError ? "border-red-300" : "border-gray-200"
+              }`}
+              placeholder="Ticket Number (optional): ABC-123, XYZ-789"
+              onKeyUp={(e) => handleKeyUp(e)}
+              onChange={() => clearErrors("ticketNumberError")}
+            />
+            <button
+              className="absolute right-0 rounded-r-md border-2 bg-[#242424] p-2"
+              onClick={() => processTickets(watch("ticket-numbers"))}
+            >
+              <PlusIcon className="h-6 w-6 text-white" />
+            </button>
+          </div>
+          {errors.ticketNumberError && (
+            <p
+              className={`${
+                tickets.length ? "-mt-3" : ""
+              } text-sm text-red-300`}
+            >
+              Ticket Number length must be less than 20 characters.
+            </p>
+          )}
+          <ul className="flex flex-wrap gap-1">
+            {tickets.map(({ id, title }) => (
+              <li key={id} className="flex gap-2">
+                <button
+                  aria-label="Close"
+                  onClick={removeTicket(id)}
+                  className="inline-flex gap-0.5 rounded-md border-2 border-blue-200 p-1 text-blue-200 transition hover:border-red-300 hover:text-red-300"
+                >
+                  <span>{title}</span>
+                  <XCircleIcon className="h-6 w-6" />
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
 
         <div className="flex">
@@ -126,7 +164,7 @@ function App() {
             {...register("body")}
             name="body"
             id="body"
-            className="basis-full resize-none rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black"
+            className="basis-full resize-none rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none"
             placeholder="Body (optional): longer description of the change"
           ></textarea>
         </div>
@@ -139,7 +177,7 @@ function App() {
             {...register("footer")}
             name="footer"
             id="footer"
-            className="basis-full resize-none rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black"
+            className="basis-full resize-none rounded-md border-2 border-gray-200 bg-transparent p-2 text-gray-200 transition hover:border-blue-200 focus:bg-gray-200 focus:text-black focus:outline-none"
             placeholder="Footer (optional): breaking changes, closed issues, etc."
           ></textarea>
         </div>
@@ -163,7 +201,7 @@ function App() {
                 {watch("footer") && <p>{watch("footer")}</p>}
               </div>
               <button
-                className="inline-flex max-w-fit gap-2 rounded-lg border-2 border-blue-200 p-2 text-blue-200 transition hover:border-green-200 hover:text-green-200"
+                className="inline-flex max-w-fit gap-2 rounded-lg border-2 border-blue-200 p-2 text-blue-200 transition hover:border-green-200 hover:text-green-200 focus:outline-none"
                 onClick={copyToClipboard}
               >
                 <p>Copy to Clipboard</p>
--- src/utils/hooks/useTickets.tsx
+++ src/utils/hooks/useTickets.tsx
@@ -8,13 +8,23 @@ export interface Ticket {
 export const useTickets = (initialState: Ticket[] = []) => {
   const [tickets, setTickets] = useState<Ticket[]>(initialState);
 
-  const addTickets = (ticketString: string) => {
+  const addTickets = (
+    ticketString: string,
+    onError: () => void,
+    onResolve: () => void
+  ) => {
     const newTickets = ticketString
       .split(",")
       .map((ticket) => ticket.trim())
       .filter((ticket) => ticket.length)
       .map((ticket) => ({ id: crypto.randomUUID(), title: ticket }));
 
+    if (newTickets.some(({ title }) => title.length > 20)) {
+      onError();
+      return;
+    }
+
+    onResolve();
     setTickets((tickets) => [...tickets, ...newTickets]);
   };
   const removeTicket = (id: string) => () => {
